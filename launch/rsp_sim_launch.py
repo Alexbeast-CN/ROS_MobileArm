ea9bca6c6ee8f8e2909ec24c5bedb8cb3ec82f1a
ygong0712 <91856596+ygong0712@users.noreply.github.com>
2023-03-11 20:03:25 -0800

    gazebo sim done

--- launch/rsp_sim_launch.py
+++ launch/rsp_sim_launch.py
@@ -1,56 +1,47 @@
 import os
+
 from ament_index_python.packages import get_package_share_directory
+
+
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
-
 from launch_ros.actions import Node
-import xacro
+
 
 
 def generate_launch_description():
 
-    # Specify the name of the package and path to xacro file within the package
-    pkg_name = 'ros_mobile_arm'
-    file_subpath = 'robot_description/robot.urdf.xacro'
 
+    # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
+    # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    # Use xacro to process the file
-    xacro_file = os.path.join(get_package_share_directory(pkg_name),file_subpath)
-    robot_description_raw = xacro.process_file(xacro_file).toxml()
+    package_name='ros_mobile_arm' #<--- CHANGE ME
 
-
-    # Configure the node
-    node_robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        output='screen',
-        parameters=[{'robot_description': robot_description_raw,
-        'use_sim_time': True}] # add other parameters here if required
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-
-
+    # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-        )
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
 
-
+    # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                    arguments=['-topic', 'robot_description',
-                                '-entity', 'my_bot'],
-                    output='screen')
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
 
 
 
-
-
-
-    # Run the node
+    # Launch them all!
     return LaunchDescription([
+        rsp,
         gazebo,
-        node_robot_state_publisher,
-        spawn_entity
+        spawn_entity,
     ])
