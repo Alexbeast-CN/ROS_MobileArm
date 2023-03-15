93c8f7cd3f6f4de47dbaa3333ba9b002d5d54f9d
Daoming Chen <chendaoming99@Outlook.ie>
2023-03-15 19:16:57 +0800

    move to robot_description

--- src/robot_description/launch/rsp.launch.py
+++ src/robot_description/launch/rsp.launch.py
@@ -16,8 +16,8 @@ def generate_launch_description():
     use_sim_time = LaunchConfiguration('use_sim_time')
 
     # Process the URDF file
-    pkg_path = os.path.join(get_package_share_directory('src'))
-    xacro_file = os.path.join(pkg_path,'robot_description','robot.urdf.xacro')
+    pkg_path = os.path.join(get_package_share_directory('robot_description'))
+    xacro_file = os.path.join(pkg_path,'urdf','robot.urdf.xacro')
     robot_description_config = xacro.process_file(xacro_file)
     
     # Create a robot_state_publisher node
--- src/robot_description/launch/rsp_sim_launch.py
+++ src/robot_description/launch/rsp_sim_launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
     # Include the robot_state_publisher launch file, provided by our own package. Force sim time to be enabled
     # !!! MAKE SURE YOU SET THE PACKAGE NAME CORRECTLY !!!
 
-    package_name='src' #<--- CHANGE ME
+    package_name='robot_description' #<--- CHANGE ME
 
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
